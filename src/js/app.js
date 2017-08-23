428450b1aab7d6e917ae79496c0a2fb1a473cdb8
ayang10 <ayang014@gmail.com>
2017-08-23 16:05:36 -0400

    finish pokedex

--- src/js/app.js
+++ src/js/app.js
@@ -1,118 +1,129 @@
+$(document).ready(function () {
 
-// $('#submitButton').click(function (){
-// // Adding loading GIF
-//  $('#content').html('<img id="loader-img" alt="" src="http://adrian-design.com/images/loading.gif" width="100" height="100" align="center" />');
- 
-//  // Ajax Request
-//  $.ajax({
-//  type: "GET",
-//  dataType: "json",
-//  url: "https://api.github.com/users/sam",
-//   success: function (loader) {
-//  // This replace the retrieved data to the div after the setTimeOut function
-//       setTimeout(function () {
-//         $('#content').html('Hi I am ' + loader.login + '!' + 'From ' +loader.location).addClass('border');
-//             }, 3000);
-//         }
-//     });
-// });
+    $(".pokeContainer, #pokeball").hide();
 
-$('.pokeContainer, #pokeball').hide();
+    $( "#submitButton" ).on("click", PokeDex);
 
-$( "#submitButton" ).on("click", PokeDex);
+    $( "#randomSearch" ).on("click", PokeDex);
 
-$('#pokeInput').keyup(function(e) {
-    if (e.keyCode == 13) {
-      $(PokeDex);
-    }
-  })
-
-function PokeDex () {
-    
-    // empty the listview
-    $("#pokeDetails").empty();
-    $('.pokeContainer, #pokeball').show();
-    var param = document.getElementById("pokeInput").value.toLowerCase();
-    var pokeURL = "http://pokeapi.co/api/v1/pokemon/" + param;
-    var pokeURL2 = "http://pokeapi.co/api/v2/pokemon/" + param;
-
-    if(param === '') {
-      $('.pokeContainer, #pokeball').hide();
-      $('#error').html('<div class="alert alert-warning"><strong>Warning!</strong> Error.  There was no entry.</div>').fadeOut(3500);
-    }
-    else{
-    $.getJSON(pokeURL, function(data){
-        //console.log(data);
-        var pokeID = data.national_id;
-        var pokeName = data.name;
-        var pokeType1 = data.types[0].name;
-        if (data.types.length == 2) {
-            var pokeType2 = data.types[1].name;
+    $("#pokeInput").keyup(function(e) {
+        if (e.keyCode == 13) {
+            PokeDex();
         }
-        else var pokeType2 = null;
-        var descriptionURI = "http://pokeapi.co" + data.descriptions[0].resource_uri;
-        var pokeDescription = "";
-        // console.log('hello', descriptionURI);
+    })
 
-        $.getJSON(descriptionURI, function(data2){
-            console.log(descriptionURI);
-            pokeDescription = data2.description;
-        });
+    function PokeDex(button, num) {
+        var pokemon = num;
+        // empty the listview
+        $(".pokeContainer, #pokeball").show();
+        var param = '';
 
-        $.getJSON(pokeURL2, function(data3){
-        
-            var imageURI = data3.sprites.front_default;
+        if (button === 'previous'){
+            param = pokemon - 1;
+        }
+        else if (button === 'next'){
+            param = pokemon + 1;
+        }
+        else if($(this).attr('id') === 'submitButton' || $(this).attr('id') === undefined){
+            param = document.getElementById('pokeInput').value.toLowerCase();
+        }
+        else{
+            param = Math.floor(Math.random() * 718) + 1;
+        }
+        pokemon = param;
+        console.log(pokemon);
+        var pokeURL = "http://pokeapi.co/api/v1/pokemon/" + param;
+        var pokeURL2 = "http://pokeapi.co/api/v2/pokemon/" + param;
 
-            console.log("Number: ", pokeID);
-            console.log("Name: ", pokeName);
-            console.log("Type 1: ", pokeType1);
-            console.log("Type 2: ", pokeType2);
-            console.log("Description URI: ", descriptionURI);
-            console.log("Description: ", pokeDescription);
-            console.log("Image URI: ", imageURI);
-
-            var div = "";
-            div += '<div style="text-align: center;"><img src="' + imageURI + '" width="150" height="150">';
-            div += '<h1>#' + pokeID + ' ' + pokeName + '</h1>';
-            div += '<p>Type 1: ' + pokeType1 + '</p>';
-
-            if (pokeType2 != null){
-                div += '<p>Type 2: ' + pokeType2 + '</p>';
+        if(param === '') {
+        $(".pokeContainer, #pokeball").hide();
+        $("#error").html('<div class="alert alert-warning"><strong>Warning!</strong> Error.  There was no entry.</div>').fadeOut(3500);
+        }
+        else{
+        $.getJSON(pokeURL, function(data){
+            //console.log(data);
+            var pokeID = data.national_id;
+            var pokeName = data.name;
+            var pokeType1 = data.types[0].name;
+            if (data.types.length == 2) {
+                var pokeType2 = data.types[1].name;
             }
+            else var pokeType2 = null;
+            var descriptionURI = "http://pokeapi.co" + data.descriptions[0].resource_uri;
+            var pokeDescription = "";
+            // console.log('hello', descriptionURI);
 
-            div += '<p>Description: ' + pokeDescription + '</p>';
-            div += '</div>';
+            $.getJSON(descriptionURI, function(data2){
+                console.log(descriptionURI);
+                pokeDescription = data2.description;
+            });
 
-            $('.pokeContainer, #pokeball').hide();
+            $.getJSON(pokeURL2, function(data3){
             
-            // $("#pokeDetails").append(div);
+                var imageURI = data3.sprites.front_default;
 
-            bootbox.dialog({
-            message: div,
-            buttons: {
-                cancel: {
-                    label: "Previous",
-                    className: 'btn-danger',
-                    callback: function(){
-                        Example.show('Custom cancel clicked');
-                    }
-                },
-                ok: {
-                    label: "Next",
-                    className: 'btn-info',
-                    callback: function(){
-                        Example.show('Custom OK clicked');
+                console.log("Number: ", pokeID);
+                console.log("Name: ", pokeName);
+                console.log("Type 1: ", pokeType1);
+                console.log("Type 2: ", pokeType2);
+                console.log("Description URI: ", descriptionURI);
+                console.log("Description: ", pokeDescription);
+                console.log("Image URI: ", imageURI);
+
+                var div = '';
+                div += '<div style="text-align: center;"><img src="' + imageURI + '" width="150" height="150">';
+                div += '<h1>#' + pokeID + ' ' + pokeName + '</h1>';
+                div += '<p>Type 1: ' + pokeType1 + '</p>';
+
+                if (pokeType2 != null){
+                    div += '<p>Type 2: ' + pokeType2 + '</p>';
+                }
+
+                div += '<p>Description: ' + pokeDescription + '</p>';
+                div += '</div>';
+
+                $(".pokeContainer, #pokeball").hide();
+                
+                PokemonModal(div, pokeID);
+            
+            });
+
+        })
+        .fail(function() {
+        $(".pokeContainer, #pokeball").hide();
+        $("#error").html('<div class="alert alert-warning"><strong>Warning!</strong> Error.  Not a valid entry.</div>').fadeOut(3500);
+    })
+        }
+    }
+
+    function PokemonModal(details, pokemon) {
+
+        bootbox.dialog({
+                message: details,
+                onEscape: function() { console.log("Escape!"); },
+                backdrop: true,
+                buttons: {
+                    confirm: {
+                        label: "Previous",
+                        className: 'btn-danger btn-previous',
+                        callback: function(){
+                           PokeDex('previous', pokemon);
+                        }
+                    },
+                    ok: {
+                        label: "Next",
+                        className: 'btn-info',
+                        callback: function(){
+                            if(pokemon === 718){
+                                return false;
+                            }
+                            else{
+                            PokeDex('next', pokemon);
+                            }
+                        }
                     }
                 }
-            }
-            });
-            
-        });
-
-    })
-    .fail(function() {
-      $('.pokeContainer, #pokeball').hide();
-      $('#error').html('<div class="alert alert-warning"><strong>Warning!</strong> Error.  Not a valid entry.</div>').fadeOut(3500);
-  })
+                });  $(".btn-previous").attr("id", "previous");
     }
-}
+
+});
